b3727844b949cb1b30d87994c56ce9f262e7b2c8
swetasmi <swetatwinkil@gmail.com>
2023-09-30 18:58:48 +0530

    account 1st commit

---
+++ src/test/java/creataccount/fundtransfer.java
@@ -0,0 +1,13 @@
+package creataccount;
+
+import org.testng.annotations.Test;
+
+public class fundtransfer {
+	
+	@Test
+	public void ac()
+	{
+		System.out.println("fund sucessfully transfer");
+	}
+
+}
